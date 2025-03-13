c281be154d5513ae401aa53d6081c12fa90828c7
Shannanaa <jannaspasyonkina@gmail.com>
2025-03-13 19:30:21 +1000

    header

--- client/src/widgets/Header/Header.tsx
+++ client/src/widgets/Header/Header.tsx
@@ -1,8 +1,65 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useAppSelector, useAppDispatch } from '../../shared/hooks/reduxHooks';
+import { selectUser } from '../../entities/user/slice/userSlice';
+import Logo from './Logo';
+import './Header.css';
+import { UserAvatar } from '../../entities/user/ui/UserAvatar/UserAvatar';
+import { signOutThunk } from "../../entities/user/api/index";
 
-export default function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Header(): JSX.Element {
+    const user = useAppSelector(selectUser);
+    const dispatch = useAppDispatch();
+
+    const [isRulesModalOpen, setIsRulesModalOpen] = useState(false);
+
+    const toggleRulesModal = () => {
+        setIsRulesModalOpen(!isRulesModalOpen);
+    };
+
+    const handleLogout = () => {
+      dispatch(signOutThunk());
+    };
+
+    return (
+        <header className="header">
+            <div className="header-content">
+                <Logo className="header-logo" />
+
+                {user ? (
+                    <div className="user-actions">
+                        <button className="rules-button" onClick={toggleRulesModal}>
+                            ?
+                        </button>
+                        <UserAvatar user={user} />
+                        <button className="logout-button" onClick={handleLogout}>
+                            Выйти
+                        </button>
+                    </div>
+                ) : (
+                    <div className="auth-buttons">
+                        <Link to="/login" className="auth-button">
+                            Войти
+                        </Link>
+                        <Link to="/register" className="auth-button">
+                            Регистрация
+                        </Link>
+                    </div>
+                )}
+            </div>
+
+            {isRulesModalOpen && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close-button" onClick={toggleRulesModal}>
+                            &times;
+                        </span>
+                        <h2>Правила игры </h2>
+                        <p>Правило 1: бла бла бла...</p>
+                        <p>Правило 2: бла бла бла...</p>
+                    </div>
+                </div>
+            )}
+        </header>
+    );
 }
