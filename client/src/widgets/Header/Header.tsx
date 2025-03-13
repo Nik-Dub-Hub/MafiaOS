84cabaa3096cb42c4a4bb11dc9b955bee2efc30c
luckytoxic <mrbalumba27@gmail.com>
2025-03-13 12:06:38 +0300

    done SignInForm and signOut logic(in comment)

--- client/src/widgets/Header/Header.tsx
+++ client/src/widgets/Header/Header.tsx
@@ -1,6 +1,20 @@
+// import { signOutThunk } from "@/entities/user"
+// import { CLIENT_ROUTES } from "@/shared/enums/clientRoutes"
+// import { useAppDispatch } from "@/shared/hooks/reduxHooks"
+// import { useNavigate } from "react-router"
 
 
 export default function Header() {
+  // const navigate = useNavigate()
+  // const dispatch = useAppDispatch()
+
+  // const onSignOutHandler = async()=>{
+  //   dispatch(signOutThunk())
+  //   //! Переделать на Alert
+  //   console.log('Вы успешно вышли из системы');
+  //   navigate(CLIENT_ROUTES.MAIN)
+  // }
+
   return (
     <div>
       
