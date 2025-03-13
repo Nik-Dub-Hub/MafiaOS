392ccda4fcf6fa4dc96560a75e90ccf6614ff17f
Nik-Dub-Hub <ioi741741@gmail.com>
2025-03-13 19:09:14 +0300

    done game RTK

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -1,10 +1,11 @@
 import { userReducer } from "@/entities/user";
 import { configureStore } from "@reduxjs/toolkit";
-
+import { gameReducer } from "@/entities/game";
 
 const store = configureStore({
     reducer:{
         user: userReducer,
+        game: gameReducer,
     }
 }) 
 
---
+++ client/src/entities/game/slice/gameSlice.ts
@@ -0,0 +1,94 @@
+import {
+    setGameThunk,
+    addGameThunk,
+    updateGameThunk,
+    deleteGameThunk,
+  } from "../api";
+  
+  import { createSlice } from "@reduxjs/toolkit";
+  import { GameArrayType } from "../model";
+  
+  type GameState = {
+    games: GameArrayType;
+    error: string | null;
+    isLoading: boolean;
+  };
+  
+  const initialState: GameState = {
+    games: [],
+    error: null,
+    isLoading: false,
+  };
+  
+  const gameSlice = createSlice({
+    name: "game",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+      builder
+        .addCase(setGameThunk.pending, (state) => {
+          state.isLoading = true;
+        })
+        .addCase(setGameThunk.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.error = null;
+          state.games = action.payload.data;
+        })
+        .addCase(setGameThunk.rejected, (state, action) => {
+          state.isLoading = false;
+          state.error = action.payload!.error ?? "Unknown error";
+          state.games = [];
+        })
+  
+        .addCase(addGameThunk.pending, (state) => {
+          state.isLoading = true;
+        })
+        .addCase(addGameThunk.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.error = null;
+          state.games = [...state.games, action.payload.data];
+        })
+        .addCase(addGameThunk.rejected, (state, action) => {
+          state.isLoading = false;
+          state.error = action.payload!.error ?? "Unknown error";
+          state.games = [];
+        })
+  
+        .addCase(updateGameThunk.pending, (state) => {
+          state.isLoading = true;
+        })
+        .addCase(updateGameThunk.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.error = null;
+          state.games = state.games.map((games) =>
+            games.id === action.payload.data.id
+              ? action.payload.data
+              : games
+          );
+        })
+        .addCase(updateGameThunk.rejected, (state, action) => {
+          state.isLoading = false;
+          state.error = action.payload!.error ?? "Unknown error";
+          state.games = [];
+        })
+  
+        .addCase(deleteGameThunk.pending, (state) => {
+          state.isLoading = true;
+        })
+        .addCase(deleteGameThunk.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.error = null;
+          state.games = state.games.filter(
+            (games) => games.id !== action.payload.data.id
+          );
+        })
+        .addCase(deleteGameThunk.rejected, (state, action) => {
+          state.isLoading = false;
+          state.error = action.payload!.error ?? "Unknown error";
+          state.games = [];
+        })
+    },
+  });
+  
+  export const gameReducer = gameSlice.reducer;
+  
