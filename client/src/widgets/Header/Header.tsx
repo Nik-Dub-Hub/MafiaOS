e09bcb64f6ad9b38d93392b8e03ed9d2f11fe7b9
luckytoxic <mrbalumba27@gmail.com>
2025-03-14 13:16:39 +0300

    done styles for header

--- client/src/widgets/Header/Header.tsx
+++ client/src/widgets/Header/Header.tsx
@@ -2,13 +2,13 @@ import { JSX, useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../../shared/hooks/reduxHooks';
 import { selectUser } from '../../entities/user/slice/userSlice';
 import Logo from './Logo';
-import './Header.css';
+import styles from'./Header.module.css';
 import { UserAvatar } from '../../entities/user/ui/UserAvatar/UserAvatar';
 import { signOutThunk } from "../../entities/user/api/index";
 import LoginModal from '../../features/modal/LoginModal';
 import RegisterModal from '../../features/modal/RegisterModal';
+import { showAlert } from '@/features/alerts';
 
-//interface HeaderProps {}
 
 export default function Header(): JSX.Element {
     const user = useAppSelector(selectUser);
@@ -24,6 +24,7 @@ export default function Header(): JSX.Element {
 
     const handleLogout = () => {
       dispatch(signOutThunk());
+      dispatch(showAlert({ message: "Вы успешно вышли,до встречи👋" ,status:'success'}));
     };
 
     const openLoginModal = () => setIsLoginModalOpen(true);
@@ -34,47 +35,61 @@ export default function Header(): JSX.Element {
 
 
     return (
-        <header className="header">
-            <div className="header-content">
-                <Logo className="header-logo" />
+      <header className={styles.header}>
+        <div className={styles.headerContent}>
+          <Logo className="header-logo" />
 
-                {user ? (
-                    <div className="user-actions">
-                        <button className="rules-button" onClick={toggleRulesModal}>
-                            ?
-                        </button>
-                        <UserAvatar user={user} />
-                        <button className="logout-button" onClick={handleLogout}>
-                            Выйти
-                        </button>
-                    </div>
-                ) : (
-                    <div className="auth-buttons">
-                        <button className="auth-button" onClick={openLoginModal}>
-                            Войти
-                        </button>
-                        <button className="auth-button" onClick={openRegisterModal}>
-                            Регистрация
-                        </button>
-                    </div>
-                )}
+          {user ? (
+            <div className={styles.userActions}>
+              <img
+                src="/public/question.svg"
+                alt="question"
+                className={styles.rulesButton}
+                onClick={toggleRulesModal}
+              />
+              <UserAvatar user={user} />
+              <img
+                src="/public/logout.svg"
+                alt="logout"
+                className={styles.logoutButton}
+                onClick={handleLogout}
+              />
             </div>
+          ) : (
+            <div className={styles.authButtons}>
+                <img
+                  src="/public/login.svg"
+                  alt="login"
+                  className={styles.authButton}
+                  onClick={openLoginModal}
+                />
+            </div>
+          )}
+        </div>
 
-            {isRulesModalOpen && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <span className="close-button" onClick={toggleRulesModal}>
-                            &times;
-                        </span>
-                        <h2>Правила игры</h2>
-                        <p>Количество игроков: ...</p>
-                        <p>КОличество фаз: ...</p>
-                    </div>
-                </div>
-            )}
+        {isRulesModalOpen && (
+          <div className={styles.modal}>
+            <div className={styles.modalContent}>
+              <span className={styles.closeButton} onClick={toggleRulesModal}>
+                &times;
+              </span>
+              <h2>Правила игры</h2>
+              <p>Количество игроков: ...</p>
+              <p>КОличество фаз: ...</p>
+            </div>
+          </div>
+        )}
 
-            <LoginModal open={isLoginModalOpen} onClose={closeLoginModal} />
-            <RegisterModal open={isRegisterModalOpen} onClose={closeRegisterModal} />
-        </header>
+        <LoginModal
+          open={isLoginModalOpen}
+          onClose={closeLoginModal}
+          onOpenRegisterModal={openRegisterModal}
+        />
+        <RegisterModal
+          open={isRegisterModalOpen}
+          onClose={closeRegisterModal}
+          openLoginModal={openLoginModal}
+        />
+      </header>
     );
 }
