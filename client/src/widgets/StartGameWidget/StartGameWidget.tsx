2ed57b2e2e2e878e980c10999164cc55fac0e65d
Nik-Dub-Hub <ioi741741@gmail.com>
2025-03-19 19:43:22 +0300

    done

--- client/src/widgets/StartGameWidget/StartGameWidget.tsx
+++ client/src/widgets/StartGameWidget/StartGameWidget.tsx
@@ -1,18 +1,22 @@
+import { useState, useEffect } from "react";
 import { useAppSelector } from "@/shared/hooks/reduxHooks";
 import Timer from "../Timer/Timer";
 import RoleCard from "@/entities/role/ui/RoleCard";
 import { useParams } from "react-router";
 import { IGame } from "@/entities/game";
 import DailyVotingWidget from "../DailyVotingWidget/DailyVotingWidget";
-
+import ResultModal from "../../features/modal/ResultModal/ResultModal";
 
 type Props = {
-  game: IGame;  
+  game: IGame;
 };
 
-export default function StartGameWidget({game}: Props) {
+export default function StartGameWidget({ game }: Props) {
   const { id } = useParams();
   const { user } = useAppSelector((state) => state.user);
+  const [winner, setWinner] = useState<"mafia" | "civilians" | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const player = useAppSelector((state) =>
     state.players.players.find(
       (p) => p.user_id === user?.id && p.game_id === Number(id)
@@ -24,32 +28,59 @@ export default function StartGameWidget({game}: Props) {
   const gamePlayers = useAppSelector((state) =>
     state.players.players.filter((el) => el.game_id === Number(id))
   );
-  // const mafiaPlayers = gamePlayers.filter((player) => player.role_id === 3);
-  if (!user) {
+  const mafiaPlayers = gamePlayers.filter(
+    (player) => player.role_id === 3 && player.isAlive
+  );
+  const civiliansPlayers = gamePlayers.filter(
+    (player) => player.role_id !== 3 && player.isAlive
+  );
+
+  useEffect(() => {
+    if (game.phase !== "Ожидание" && game.phase !== "Знакомство") {
+      if (mafiaPlayers.length === 0 && civiliansPlayers.length > 0) {
+        setWinner("civilians");
+        setIsModalOpen(true);
+      } else if (civiliansPlayers.length <= mafiaPlayers.length) {
+        setWinner("mafia");
+        setIsModalOpen(true);
+      } else if (player && !player.isAlive) {
+        setIsModalOpen(true);
+      }
+    }
+  }, [game.phase, player, mafiaPlayers.length, civiliansPlayers.length]);
+
+  if (!user || !player) {
     return <div>Пользователь не авторизован</div>;
   }
- return (
-   <div>
-     <Timer gamePlayers={gamePlayers} game={game} user={user} />
-     {(game.phase === "Знакомство" || game.phase === "Обсуждение") && (
-       <RoleCard role={role} />
-     )}
-     {game.phase === "Ночное голосование" && (
-       <>
-         {player?.role_id === 3 ? (
-           <DailyVotingWidget
-             gamePlayers={gamePlayers}
-             game={game}
-             user={user}
-           />
-         ) : (
-           <RoleCard role={role} />
-         )}
-       </>
-     )}
-     {game.phase === "Дневное голосование" && (
-       <DailyVotingWidget gamePlayers={gamePlayers} game={game} user={user} />
-     )}
-   </div>
- );
+
+  return (
+    <div>
+      <Timer gamePlayers={gamePlayers} game={game} user={user} />
+      {(game.phase === "Знакомство" || game.phase === "Обсуждение") && (
+        <RoleCard role={role} />
+      )}
+      {game.phase === "Ночное голосование" && (
+        <>
+          {player.role_id === 3 ? (
+            <DailyVotingWidget
+              gamePlayers={gamePlayers}
+              game={game}
+              user={user}
+            />
+          ) : (
+            <RoleCard role={role} />
+          )}
+        </>
+      )}
+      {game.phase === "Дневное голосование" && (
+        <DailyVotingWidget gamePlayers={gamePlayers} game={game} user={user} />
+      )}
+      <ResultModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        player={player}
+        winner={winner}
+      />
+    </div>
+  );
 }
